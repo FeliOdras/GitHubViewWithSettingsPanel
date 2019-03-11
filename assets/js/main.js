94af75c7e0f5e8b7a86ca9200c43c1754927c46d
FeliOdras <ariane.feldmann@odras.de>
2019-03-11 11:54:46 +0100

    Added current time aoutpu method

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,19 @@ class ShowCurrentTime {
     constructor(domselector) {
         this.Date = new Date()
         this.htmlContainer = document.querySelector(domselector)
+        this.currentTimeOutput();
+    }
+
+    currentTimeOutput() {
+        let currHour = this.Date.getHours();
+        let currMinute = this.Date.getMinutes();
+        let currSecond = this.Date.getSeconds();
+        let currTime = `
+            ${currHour < 10 ? `0`:``}${currHour}:
+            ${currMinute < 10 ? `0`:``}${currMinute}:
+            ${currSecond < 10 ? `0`:``}${currSecond}
+            `
+        return currTime;
     }
 }
 const showCurrentTime = new ShowCurrentTime('.currentTime');
