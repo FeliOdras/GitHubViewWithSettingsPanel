bc6534af4971403caff725bf8512e9293ed23e63
odras <ariane.feldmann@odras.de>
2019-03-11 18:18:44 +0100

    Removed old approach to display time

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,57 +5,6 @@ class ShowCurrentTime {
         this.render();
     }
 
-    template() {
-    //     setInterval(function(){
-    //     let currMonth = this.Date.getMonth();
-    //     currMonth == 0 ? currMonth = 'January' :
-    //         currMonth == 1 ? currMonth = 'February' :
-    //         currMonth == 2 ? currMonth = 'March' :
-    //         currMonth == 3 ? currMonth = 'April' :
-    //         currMonth == 4 ? currMonth = 'May' :
-    //         currMonth == 5 ? currMonth = 'June' :
-    //         currMonth == 6 ? currMonth = 'July' :
-    //         currMonth == 7 ? currMonth = 'August' :
-    //         currMonth == 8 ? currMonth = 'September' :
-    //         currMonth == 9 ? currMonth = 'October' :
-    //         currMonth == 10 ? currMonth = 'November' :
-    //         currMonth == 11 ? currMonth = 'December' :
-    //         ''
-    //     let currDay = this.Date.getDate();
-    //     let currWeekday = this.Date.getDay();
-    //     currWeekday == 0 ? currWeekday = 'Sunday' :
-    //         currWeekday == 1 ? currWeekday = 'Monday' :
-    //         currWeekday == 2 ? currWeekday = 'Tuesday' :
-    //         currWeekday == 3 ? currWeekday = 'Wednesday' :
-    //         currWeekday == 4 ? currWeekday = 'Thursday' :
-    //         currWeekday == 5 ? currWeekday = 'Friday' :
-    //         currWeekday == 6 ? currWeekday = 'Saturday' :
-    //         ''
-    //     let currYear = this.Date.getFullYear();
-    //     let currHour = this.Date.getHours();
-    //     let currMinute = this.Date.getMinutes();
-        
-    //         let currSecond = this.Date.get();
-    //     let currTime = `
-    //         <div class="date">
-    //         ${currWeekday}, ${currMonth}
-    //         ${currDay}${currDay == 1 || currDay == 21 || currDay == 31 ? 'st' 
-    //         : currDay == 2 || currDay == 22 ? 'nd' 
-    //         : currDay == 3 || currDay == 23 ? 'rd' 
-    //         : 'th'}
-    //         ${currYear}
-    //         </div>
-    //         <div class="hour">
-    //         ${currHour < 10 ? `0`:``}${currHour}:${currMinute < 10 ? `0`:``}${currMinute}:${currSecond}
-            
-    //         `;
-    //     return currTime;
-    // },
-    // 1000
-    // )
-    return 'Hello date'
-    }
-
     getNowDate(){
         let currentDate = moment().format('dddd, MMMM Do YYYY');
         return currentDate;
@@ -71,7 +20,6 @@ class ShowCurrentTime {
     render() {
         const template = this.template();
         let currentDate = this.getNowDate();
-        console.log(currentDate);
         let output = ``;
         output += `<div class="date">`;
         output += `${currentDate}`;
